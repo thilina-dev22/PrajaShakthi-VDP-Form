dd5b42853b70cb07761e4875dabd507309bed86a
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-10-30 14:19:38 +0530

    Revert "feat: Implement password management features"

--- PrajaShakthi-VDP-Form-frontend/src/components/AppRoutes.jsx
+++ PrajaShakthi-VDP-Form-frontend/src/components/AppRoutes.jsx
@@ -7,7 +7,6 @@ import DevelopmentForm from './DevelopmentForm';
 import UserManagement from './UserManagement';
 import ActivityLogs from './ActivityLogs';
 import NotificationsPage from './NotificationsPage';
-import PasswordManagement from './PasswordManagement';
 import Navigation from './Navigation';
 
 const AppRoutes = () => {
@@ -50,8 +49,6 @@ const renderRoute = (route, isSuperAdmin, isDistrictAdmin, isDSUser, setCurrentR
         return <DevelopmentForm setCurrentRoute={setCurrentRoute} />;
       case 'submissions':
         return <SubmissionList />;
-      case 'password':
-        return <PasswordManagement />;
       default:
         return <CommunityCouncilForm setCurrentRoute={setCurrentRoute} />;
     }
@@ -68,8 +65,6 @@ const renderRoute = (route, isSuperAdmin, isDistrictAdmin, isDSUser, setCurrentR
         return <ActivityLogs />;
       case 'notifications':
         return <NotificationsPage />;
-      case 'password':
-        return <PasswordManagement />;
       default:
         return <SubmissionList />;
     }
