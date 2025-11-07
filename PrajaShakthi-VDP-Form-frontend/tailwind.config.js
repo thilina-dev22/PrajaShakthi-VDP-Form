9c9c0e0c35baf47259b336a01070885d71f1285b
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-11-07 11:28:48 +0530

    Add deployment steps to VM deployment guide

--- PrajaShakthi-VDP-Form-frontend/tailwind.config.js
+++ PrajaShakthi-VDP-Form-frontend/tailwind.config.js
@@ -1,22 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'prajashakthi': {
-          'maroon': '#A8234A',
-          'orange': '#F37021',
-          'maroon-dark': '#8B1C3D',
-          'orange-dark': '#D65F1A',
-          'cream': '#F5E6D3',
-          'cream-light': '#FFF8E7',
-        }
-      }
-    },
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'prajashakthi': {
+          'maroon': '#A8234A',
+          'orange': '#F37021',
+          'maroon-dark': '#8B1C3D',
+          'orange-dark': '#D65F1A',
+          'cream': '#F5E6D3',
+          'cream-light': '#FFF8E7',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
